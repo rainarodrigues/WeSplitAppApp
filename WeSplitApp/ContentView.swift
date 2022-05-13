e88fe6d8fedfad979a3a6aff231a8a898c764986
Raina Rodrigues de Lima <rainarodriguesdelima@RainaRodrigues.local>
2022-05-13 11:41:35 -0300

    first commit

---
+++ WeSplitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplitApp
+//
+//  Created by Raina Rodrigues de Lima on 13/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Raina Rodrigues de Lima on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
